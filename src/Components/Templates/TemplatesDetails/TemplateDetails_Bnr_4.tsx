1c4cbc162055529df6ffd46e21486399636e367b
Pedro <86964318+Pedrosaint@users.noreply.github.com>
2025-03-04 10:04:13 -0800

    updated

--- src/Components/Templates/TemplatesDetails/TemplateDetails_Bnr_4.tsx
+++ src/Components/Templates/TemplatesDetails/TemplateDetails_Bnr_4.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const TemplateDetails_Bnr_4:React.FC = () => {
+const TemplateDetails_Bnr_4: React.FC = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="p-6 bg-blue-100 max-w-7xl mx-auto rounded-xl">
+      <div className="">
+        <h2 className="text-2xl font-bold text-gray-900">
+          Letterhead Templates by Headerfy
+        </h2>
+        <p className="text-gray-700 mt-3">
+          Want your business communication to stand out? Start utilizing
+          branded, professional letterhead templates to send out internal
+          notices, letters, emails, newsletters and more. Customize one of
+          Headerfy’s professionally designed letterhead templates to get
+          started.
+        </p>
+        <p className="text-gray-700 mt-5">
+          With Headerfy’s design editor, you can fully customize any template to
+          match your brand fonts and colors and update any information to fit
+          your business needs. Choose from Visme’s design library full of
+          shapes, icons, photos and more to decorate your letterhead.
+        </p>
+        <button className="mt-6 bg-red-500 text-white px-6 py-3 rounded-lg font-semibold shadow-md hover:bg-red-600 transition">
+          Create Your Letterhead Now
+        </button>
+      </div>
+    </section>
+  );
+};
 
-export default TemplateDetails_Bnr_4
+export default TemplateDetails_Bnr_4;
