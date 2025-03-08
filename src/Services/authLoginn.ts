a1cdf3faaaccee56c7471c4e10afee4575590f46
Pedro <86964318+Pedrosaint@users.noreply.github.com>
2025-03-08 00:18:51 -0800

    removed unnecessary code

--- src/Services/authLoginn.ts
+++ src/Services/authLoginn.ts
@@ -1,26 +1,6 @@
 // src/services/authService.ts
 import { apiRequest } from "../utils/AuthServicesTwo";
 
-// Define the user data type
-interface SignupData {
-  email: string;
-  phone: string;
-  fullName: string;
-  password: string;
-}
-
-// Define the expected API response type
-interface SignupResponse {
-  success: boolean;
-  message: string;
-  token?: string;
-  user?: {
-    id: number;
-    email: string;
-    fullName: string;
-  };
-}
-
 interface LoginData {
   email: string;
   password: string;
